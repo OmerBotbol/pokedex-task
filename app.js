ca447cfe58c66c8f86ee0d862c94ab5b751f217c
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-03-20 20:07:48 +0200

    move all app to root

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const api = require("./routes");
+const path = require('path')
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get("/", (req, res) => {
-    res.sendFile("./build");
-})
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use("/api", api);
 
