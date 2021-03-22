a806cde6e4e15ce2128c0bd370881945747d6e18
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-03-22 18:26:48 +0200

    PokemonDetails functional

--- client/src/components/PokemonDetails.js
+++ client/src/components/PokemonDetails.js
@@ -1,58 +1,52 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Type from './Type'
 import axios from 'axios'
 
-class PokemonDetails extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            imgSrc:this.props.pokemon.sprites.front,
+function PokemonDetails(props) {
+        const [state, setState] = useState({
+            imgSrc: props.pokemon.sprites.front,
             hover: false,
             catched: false,
-        }
-        this.switchImg = this.switchImg.bind(this)
-    }
+        });
 
-    componentDidUpdate() {
-        if(this.state.imgSrc !== this.props.pokemon.sprites.front && this.state.imgSrc !== this.props.pokemon.sprites.back){
-            this.setState({imgSrc: this.props.pokemon.sprites.front})
+    useEffect(() => {
+        if(state.imgSrc !== props.pokemon.sprites.front && state.imgSrc !== props.pokemon.sprites.back){
+            setState({imgSrc: props.pokemon.sprites.front})
         }
-    }
+    });
     
-    switchImg() {
-        if(this.state.hover) {
-            return this.setState({imgSrc: this.props.pokemon.sprites.front, hover: false});
+    function switchImg() {
+        if(state.hover) {
+            return setState({imgSrc: props.pokemon.sprites.front, hover: false});
         }
-        return this.setState({imgSrc: this.props.pokemon.sprites.back, hover: true});
+        return setState({imgSrc: props.pokemon.sprites.back, hover: true});
     }
 
-    async pokemonInCollection() {
+    async function pokemonInCollection() {
         const {data: collection} = await axios.get("/api/collection");
         console.log("collection: ", collection);
-        const isInCollection = collection.some(pokemon => pokemon.id === this.props.pokemon.id);
-        this.setState({catched: isInCollection});
+        const isInCollection = collection.some(pokemon => pokemon.id === props.pokemon.id);
+        setState({catched: isInCollection});
     }
 
-    async catch() {
-        await axios.post(`/api/collection/catch/${this.props.pokemon.name}`);
-        this.props.checkCatch();
+    async function catchPokemon() {
+        await axios.post(`/api/collection/catch/${props.pokemon.name}`);
+        props.checkCatch();
     }
-    async release() {
-        await axios.delete(`/api/collection/release/${this.props.pokemon.name}`);
-        this.props.checkCatch();
+    async function release() {
+        await axios.delete(`/api/collection/release/${props.pokemon.name}`);
+        props.checkCatch();
     }
 
-    render() {
-        return (
-            <ul className="details">
-                <li>Name: {this.props.pokemon.name}</li>
-                <li>Height {this.props.pokemon.height}</li>
-                <li>Weight: {this.props.pokemon.weight}</li>
-                <li>Types: {this.props.pokemon.types.map((type, i)=><Type key={i} showType={()=>this.props.showType(type)} type={type}/>)}</li>
-                <img src={this.state.imgSrc} alt={this.props.pokemon.name} onMouseEnter={()=>this.switchImg()} onMouseOut={()=>this.switchImg()}/>
-                <button onClick={()=>{this.props.catched ? this.release() : this.catch()}}>{this.props.catched ? "Release" : "Catch"}</button>
-        </ul>);
-    }
+    return (
+        <ul className="details">
+            <li>Name: {props.pokemon.name}</li>
+            <li>Height {props.pokemon.height}</li>
+            <li>Weight: {props.pokemon.weight}</li>
+            <li>Types: {props.pokemon.types.map((type, i)=><Type key={i} showType={()=>props.showType(type)} type={type}/>)}</li>
+            <img src={state.imgSrc} alt={props.pokemon.name} onMouseEnter={()=>switchImg()} onMouseOut={()=>switchImg()}/>
+            <button onClick={()=>{props.catched ? release() : catchPokemon()}}>{props.catched ? "Release" : "Catch"}</button>
+    </ul>);
 }
 
 export default PokemonDetails;
